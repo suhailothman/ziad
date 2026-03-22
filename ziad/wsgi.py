84a1f3f2a13082fd8a9ed8335b9b9593f81f9489
suhailothman <suhail19952018@gmail.com>
2026-03-22 04:16:22 +0300

    Delete ziad/wsgi.py

--- ziad/wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.wsgi import get_wsgi_application
-
-# تأكد أن المسار يطابق إعداداتك (ziad.settings)
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ziad.ziad.settings')
-
-application = get_wsgi_application()
